0c049a6e21a11f03bf043f2ead131fc6f230fb31
CifLord <rtr53829@gmail.com>
2025-08-01 17:47:46 +0200

    change positional arguments

--- scripts/hf_small_batch_script.py
+++ scripts/hf_small_batch_script.py
@@ -60,11 +60,11 @@ if __name__=="__main__":
         SlurmPipelineExecutor(
             pipeline=[
                 RunChgcarWF(
-                    bucket_name=bucket_name, 
-                    aws_access_key_id=aws_access_key_id, 
-                    aws_secret_access_key=aws_secret_access_key,
-                    region_name=region_name, 
-                    metadata=metadata
+                    bucket_name, 
+                    aws_access_key_id, 
+                    aws_secret_access_key,
+                    region_name, 
+                    metadata
                 )
             ],
             job_name="small_batch_RunChgcarWF",
