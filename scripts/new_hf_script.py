277b8c17d4a2f84ff77f1a481e85a1b633a21157
CifLord <rtr53829@gmail.com>
2025-12-05 20:38:04 +0100

    added new script after martin's fix

---
+++ scripts/new_hf_script.py
@@ -0,0 +1,87 @@
+from datatrove.executor import SlurmPipelineExecutor
+
+from upload_to_aws import RunChgcarWF
+
+import argparse, json
+
+
+def read_options():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--cpus_per_task", dest="cpus_per_task", type=str, 
+                        help="cpus_per_task")
+    parser.add_argument("-p", "--partition", dest="partition", type=str, 
+                        help="partition")
+    parser.add_argument("-t", "--time", dest="time", type=str, 
+                        help="time")
+    parser.add_argument("-b", "--bucket_name", dest="bucket_name", type=str, 
+                        help="name of aws s3 bucket")
+    parser.add_argument("-a", "--aws_access_key_id", dest="aws_access_key_id", type=str, 
+                        help="aws s3 access ID")
+    parser.add_argument("-s", "--aws_secret_access_key", dest="aws_secret_access_key", type=str, 
+                        help="aws s3 secret access key")
+    parser.add_argument("-r", "--region_name", dest="region_name", type=str, 
+                        help="aws s3 region name")
+    parser.add_argument("-f", "--batch_file", dest="batch_file", type=str, 
+                        help="File containing dicts of metadata")
+    parser.add_argument("-l", "--logdir", dest="logdir", type=str, 
+                        help="Directory to log outputs")
+    parser.add_argument("-e", "--env_command", dest="env_command", type=str, 
+                        help="Location of file to activate environment")
+    
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__=="__main__":
+
+    args = read_options() 
+
+    # batch script inputs
+    cpus_per_task = args.cpus_per_task
+    partition = args.partition
+    t = args.time
+    all_env_commands = ["conda activate rho", 'source %s' %(args.env_command), # @Georgia, please check if this was the correct sequence to run the environment activation
+                        'export PATH=/fsx/georgia_channing/VASP/exe/IntelMPI2019/Linux-x86_64/bin:$PATH',
+                        'export PATH=/fsx/georgia_channing/VASP/exe/vasp6.4.3_cml/Linux-x86_64:$PATH',
+                        'export LD_LIBRARY_PATH=/fsx/georgia_channing/VASP/exe/IntelMPI2019/Linux-x86_64/libfabric/lib/prov:/fsx/georgia_channing/VASP/exe/IntelMPI2019/Linux-x86_64/libfabric/lib:/fsx/georgia_channing/VASP/exe/IntelMPI2019/Linux-x86_64/lib:$LD_LIBRARY_PATH',
+                        'which vasp_std',
+                        'which mpirun'
+                       ]
+    env_command =' && '.join(all_env_commands)
+    
+    logdir = args.logdir
+
+    # inputs for jobflow workflow
+    bucket_name = args.bucket_name
+    aws_access_key_id = args.aws_access_key_id
+    aws_secret_access_key = args.aws_secret_access_key
+    region_name = args.region_name
+    batch_file = args.batch_file
+
+    metadata_batch = json.load(open(batch_file, 'r'))
+
+    SlurmPipelineExecutor(
+        pipeline=[
+            RunChgcarWF(
+                bucket_name, 
+                aws_access_key_id, 
+                aws_secret_access_key,
+                region_name, 
+                metadata_batch,
+            )
+        ],
+        job_name="small_batch_RunChgcarWF",
+        logging_dir=logdir,
+        partition=partition,
+        sbatch_args={"qos": 'normal',
+            "mem-per-cpu": "2G"
+        },
+        env_command=env_command,
+        cpus_per_task=cpus_per_task,
+        tasks=len(metadata_batch),
+        max_array_launch_parallel=True,
+        time=t,
+    ).run()
